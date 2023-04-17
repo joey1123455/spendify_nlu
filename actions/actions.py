77f4c2f9da9f357f1b3c3df118d94359114b48b9
joey <folayanjoey@gmail.com>
2023-04-17 23:28:54 +0100

    model 0.2, updates to synonyms intents and data extraction

--- actions/actions.py
+++ actions/actions.py
@@ -7,21 +7,46 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
 #
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
 #
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+class AddIncomeAction(Action):
+
+    def name(self) -> Text:
+        return "add_income"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        income = tracker.get_slot("amount")
+        source = tracker.get_slot("income_category")
+        date = tracker.get_slot("date")
+        details = tracker.get_slot("income_additional_details")
+        print(income)
+        print(source)
+        print(date)
+        print(details)
+        return []
+
+class AddExpenseAction(Action):
+
+    def name(self) -> Text:
+        return "add_expense"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        item = tracker.get_slot("expense_item")
+        expense_amount = tracker.get_slot("expense_amount")
+        date = tracker.get_slot("date")
+        details = tracker.get_slot("expense_additional_details")
+        print(item)
+        for i in item:
+            print(i)
+        print(expense_amount)
+        print(date)
+        print(details)
+        return []
